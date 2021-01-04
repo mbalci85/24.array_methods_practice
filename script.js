98e8aa6790ab497a55b1e5ebb4fa163488ba0696
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-01-04 23:40:44 +0000

    added some methods and more style

--- script.js
+++ script.js
@@ -21,3 +21,29 @@ const webTechs = [
 	'Node',
 	'MongoDB',
 ];
+
+if (countries.includes('Ethiopia')) {
+	console.log('ETHIOPIA');
+} else {
+	countries.push('Ethiopia');
+}
+
+if (webTechs.includes('Sass')) {
+	console.log('Sass is a CSS preprocess');
+} else {
+	webTechs.push('Sass');
+	console.log(webTechs);
+}
+
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux'];
+const backEnd = ['Node', 'Express', 'MongoDB'];
+
+let fullStack = frontEnd.concat(backEnd);
+console.log(fullStack);
+
+let text =
+	'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.';
+
+let newText = text.replace(/[.,]/g, '').split(' ');
+console.log(newText);
+console.log(newText.length);
